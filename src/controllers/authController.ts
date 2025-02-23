49415706c90fa0557304692ef49655e2215f923e
Sergio Ayerdis <sayerdis@cln1890.com>
2025-02-23 02:30:58 -0600

    confirmation message

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,7 +4,7 @@ import { loginUserService, registerUserService } from '../services/authService';
 export const registerUser = async (req: Request, res: Response) => {
     try {
         const user = await registerUserService(req.body);
-        res.status(201).json(user);
+        res.status(201).json({user, message: 'User created successfully'});
     } catch (error: any) {
         res.status(500).json({ message: error.message });
     }
@@ -13,7 +13,7 @@ export const registerUser = async (req: Request, res: Response) => {
 export const loginUser = async (req: Request, res: Response) => {
     try {
         const token = await loginUserService(req.body);
-        res.status(200).json({token});
+        res.status(200).json({token, message: 'Login successful'});
     } catch (error: any) {
         res.status(401).json({ message: error.message });
     }
